ddd168d1fa7e253086408acba8d133dd530a794c
gomhan7 <mjjun6088@naver.com>
2025-05-31 19:15:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,14 +13,6 @@ VEC_PATH = "tfidf_vectorizer.pkl"
 st.set_page_config(page_title="낚시성 뉴스 판별기", page_icon="🎣", layout="centered")
 
 # 워터마크 제거
-hide_st_style = """
-    <style>
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-    header {visibility: hidden;}
-    </style>
-"""
-st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # --- 모델 및 벡터라이저 로딩 ---
 @st.cache_resource
