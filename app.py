afee10eddcb727cf65cc0ccb0c5dbf6e572033ba
gomhan7 <mjjun6088@naver.com>
2025-05-31 18:41:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -388,11 +388,17 @@ with col_btn2:
 
         # --- 결과 출력 ---
         st.markdown("---")
-        st.markdown("### 📊 판별 결과")
+        st.markdown(
+        f"<p style='font-size:18px;'>📊 판별 결과: <strong>{prob*100:.2f}%</strong></p>",
+        unsafe_allow_html=True
+        )
         
         # 결과 메시지를 강조하고 이모지로 시각화
         if predicted_label == 1: # 모델이 낚시성(1)으로 예측한 경우
-            st.markdown(f"## 🚨 이 뉴스는 **낚시성 뉴스**일 확률이 `{percent_clickbait}%` 입니다!")
+            st.markdown(
+            f"<p style='font-size:18px;'>🚨 이 뉴스는 **낚시성 뉴스**일 확률이 `{percent_clickbait}%` 입니다!: <strong>{prob*100:.2f}%</strong></p>",
+            unsafe_allow_html=True
+            )   
             st.error("❗ **높은 확률로 독자의 클릭을 유도하는 요소를 포함하고 있습니다.**")
             if percent_clickbait > 70:
                 st.caption("주의! 자극적인 표현이나 과장된 내용이 있을 수 있습니다.")
