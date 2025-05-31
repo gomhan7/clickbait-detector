1489a83f445ef2731d306bf02893fc3296e283f3
gomhan7 <mjjun6088@naver.com>
2025-05-31 20:25:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -161,12 +161,11 @@ def extract_info_from_url(url):
 
 
 # --- Streamlit 앱 UI ---
-st.header("📰 낚시성 뉴스 판별기")
 st.markdown(
     """
-    <div>
+    <div style="display: flex; justify-content: space-between; align-items: center;">
         <span>뉴스 제목, 본문 또는 기사 링크를 입력하면 AI가 뉴스의 낚시성 정도를 분석해드립니다.</span>
-        <img src="https://i.imgur.com/VWthizR.png" width="20" style="margin-left: 30px;" />
+        <img src="https://i.imgur.com/VWthizR.png" width="20" style="margin-left: 10px;" />
     </div>
     """,
     unsafe_allow_html=True
